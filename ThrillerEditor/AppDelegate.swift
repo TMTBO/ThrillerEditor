adf69b83b75a2c83491d9cf3f8d909e1dda02c55
Thriller <wujie@lizhi.fm>
2019-05-29 23:53:36 +0800

    Initial Commit

---
+++ ThrillerEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ThrillerEditor
+//
+//  Created by Thriller on 2019/5/29.
+//  Copyright © 2019 Thriller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ThrillerEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ThrillerEditor
+//
+//  Created by Thriller on 2019/5/29.
+//  Copyright © 2019 Thriller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
